3a30c80130d359ca72642feb2fa50039db3b01c7
exldna <dm.a.titov@ya.ru>
2022-08-15 21:12:57 +0300

    simple get states functionality

---
+++ api/cxx/src/api.cxx
@@ -0,0 +1,1 @@
+# include <chair/api.hxx>
--- api/cxx/src/chair.cxx
+++ api/cxx/src/chair.cxx
@@ -2,13 +2,7 @@
 # include <config/version.hxx>
 
 # include <curl/curl.h>
-# include <cpr/cpr.h>
-
-# include <iostream>
-
-using utils::types::deffer_t;
 
 utils::Version chair::get_version() {
-    std::cout << cpr::Get(cpr::Url{ "https://api.blockchair.com/stats" }).text << '\n';
     return { CHAIR_VERSION };
 }
